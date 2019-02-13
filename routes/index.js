1d9fff7c358399c780e9ff1326084b445bfc06b4
Frank <parmarfrank444@gmail.com>
2019-02-13 16:02:35 -0500

    Added ExpressJS Support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+router.get('/About', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+router.get('/Produts', function(req, res, next) {
+  res.render('index', { title: 'Produts' });
+});
+router.get('/Services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+router.get('/Contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
+ 
